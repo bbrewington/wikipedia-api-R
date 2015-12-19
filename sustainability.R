62190b1ce856680e78783a31ee07694f12a4a992
Brent Brewington <brent.brewington.github@gmail.com>
2015-12-19 17:13:11 -0500

    fixed top_n problem (generalized)

--- sustainability.R
+++ sustainability.R
@@ -37,10 +37,12 @@ sustainability %<>% mutate(weekday = wday(mydate),
                                     year = year(mydate),
                                     quarter = quarter(mydate))
 
+num.top <- 15
+
 page.volume <- sustainability %>% 
      group_by(page) %>% 
      summarise(mean.views = mean(views)) %>% 
-     top_n(15, mean.views) %>% mutate(volume = "top")
+     top_n(num.top, mean.views) %>% mutate(volume = "top")
      
 sustainability %<>% left_join(page.volume, by="page")
 
@@ -54,5 +56,5 @@ week.xref <- data.frame(start.date = date.seq,
 ggplot(sustainability %>% filter(volume == "top") %>% group_by(week,page) %>% summarise(weekly.views = sum(views)) %>% left_join(week.xref, by="week"),
             aes(start.date, weekly.views)) +
      geom_line(size=1) + facet_wrap(~page) + ylab("Weekly Page Views") + xlab("Month") +
-     ggtitle("Weekly View count of Top 5 Sustainability Articles (2015 Aug - Dec)")
+     ggtitle(paste0("Weekly View count of Top ",num.top," Sustainability Articles (2015 Aug - Dec)"))
 
